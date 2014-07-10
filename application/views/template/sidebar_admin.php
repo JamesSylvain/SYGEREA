8a657456c83206941af5a2d1d415b588dad35cac
James <jameswylx@gmail.com>
2014-07-10 10:26:24 +0100

    modification du sous menu admin ahjout des fichirs de langue

--- application/views/template/sidebar_admin.php
+++ application/views/template/sidebar_admin.php
@@ -1,6 +1,5 @@
 <ul class="box">
 	<li><a href="<?php echo base_url() . 'auth/'; ?>">gestion des utilisateurs</a></li>
-	<li><a href="<?php echo base_url() . 'bailleur/financement'; ?>">gestion des groups</a></li>
-	<li><a href="<?php echo base_url() . 'bailleur/financementO'; ?>">Mon compte</a></li>
-	<li><a href="<?php echo base_url() . 'projet/'; ?>">Projets</a></li>
+	<li><a href="<?php echo base_url() . 'auth/groups'; ?>">gestion des groups</a></li>
+	<li><a href="<?php echo base_url() . 'auth/myaccount'; ?>">Mon compte</a></li>
 </ul>
